c403faebb63e0b1c8cd8cbe1e4f4a66b5e97f2e3
Oleg Suzdalev <odsuzdalev@gmail.com>
2023-06-10 22:33:24 +0900

    hotfix for database insert from InlineButtons using same message_id

--- src/common/database/utils.py
+++ src/common/database/utils.py
@@ -233,10 +233,42 @@ def insert_message(message_id: int, user_id: int, text: str) -> None:
     """Data collection"""
     logger_tl_db.debug("insert_message()")
     logger_tl_db.debug(f"inserting: {message_id}, {user_id}, {text}")
+
     with create_db_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute(
-            "insert into messages (message_id, user_id, message_text) values (%s, %s, %s)",
-            (message_id, user_id, text),
-        )
-        conn.commit()
+
+        try:
+            # Try to insert the message
+            cursor.execute(
+                "insert into messages (message_id, user_id, message_text) values (%s, %s, %s)",
+                (message_id, user_id, text),
+            )
+            # If successful, commit the transaction
+            conn.commit()
+        # TODO: Needs to handle InlineButton Callbacks that send a new message using the same message_id
+        # this hotfix works for now but needs to be improved
+        except psycopg.errors.UniqueViolation:
+            # Rollback the transaction
+            conn.rollback()
+
+            # Increment the message_id
+            message_id += 1
+            print(f"UniqueViolation occurred. Retrying with incremented message_id {message_id}")
+
+            # Try to insert the message again
+            try:
+                cursor.execute(
+                    "insert into messages (message_id, user_id, message_text) values (%s, %s, %s)",
+                    (message_id, user_id, text),
+                )
+                # Commit the transaction
+                conn.commit()
+            except Exception as ex:
+                # Handle or log any exception that occurred in the second attempt
+                print(f"An error occurred during retry: {ex}")
+
+        except Exception as ex:
+            # Handle or log any other exception
+            print(f"An unexpected error occurred: {ex}")
+            conn.rollback()
+
