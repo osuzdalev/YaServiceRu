c65e17283e0381b74624723da2f901aa2ad519b6
Oleg Suzdalev <odsuzdalev@gmail.com>
2023-10-03 17:34:12 +0900

    Updated vector database 

---
+++ docker/run_compose.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Use the script like this:
+# ./run_compose.sh ../config/app/dev.yaml
+
+set -e
+
+CONFIG_FILE=${1:-config.yaml}
+
+# Autogenerate the environment variables and write them to a file
+ENV_FILE=$(mktemp)
+python3 generate_env_file.py --config $CONFIG_FILE --output $ENV_FILE
+cat $ENV_FILE
+source $ENV_FILE
+
+# Launch docker-compose
+docker-compose up -d
+
+
+echo "Docker containers started successfully!"
